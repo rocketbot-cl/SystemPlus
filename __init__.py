58b77f8b27028504217ad3f2e4ffec04ffa81110
Danilo <danilo.toro@usach.cl>
2020-02-21 14:13:39 -0300

    first commit

---
+++ __init__.py
@@ -0,0 +1,82 @@
+# coding: utf-8
+"""
+Base para desarrollo de modulos externos.
+Para obtener el modulo/Funcion que se esta llamando:
+     GetParams("module")
+
+Para obtener las variables enviadas desde formulario/comando Rocketbot:
+    var = GetParams(variable)
+    Las "variable" se define en forms del archivo package.json
+
+Para modificar la variable de Rocketbot:
+    SetVar(Variable_Rocketbot, "dato")
+
+Para obtener una variable de Rocketbot:
+    var = GetVar(Variable_Rocketbot)
+
+Para obtener la Opcion seleccionada:
+    opcion = GetParams("option")
+
+
+Para instalar librerias se debe ingresar por terminal a la carpeta "libs"
+    
+    pip install <package> -t .
+
+"""
+
+import sys
+
+"""
+    Obtengo el modulo que fueron invocados
+"""
+
+module = GetParams("module")
+
+if module == "export":
+    path = GetParams("path")
+
+    with open(path, 'w') as f:
+        f.write(str(vars_))
+
+if module == "setVariable":
+    data = GetParams("data")
+    variables = GetParams("vars")
+
+    try:
+        data = eval(data)
+        variables =  variables.split(",")
+
+        for i, var in enumerate(variables):
+            SetVar(var, data[i])
+    except Exception as e:
+        PrintException()
+        raise e
+
+if module == "backup":
+    try:
+        try:
+            os.mkdir("Logs")
+        except:
+            pass
+        name = "Logs/app_{}.log".format(datetime.now()).replace(":", ".")
+        os.rename("app.log", name)
+    except Exception as e:
+        PrintException()
+        raise e
+
+
+
+
+# if module == "import":
+#     path = GetParams("path")
+
+#     with open(path, 'r') as f:
+#         var = f.read()
+#         variables = eval(var)
+
+#     for v in variables:
+#         v.pop('$$hashKey')
+    
+
+#     vars_ = variables
+#     print(vars_)
